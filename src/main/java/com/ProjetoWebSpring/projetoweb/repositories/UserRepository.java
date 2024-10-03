bc3591cc4ad66f91b64761f2b78f32438cb0a6dd
Bruno Volpe <bruno.volpe2015@gmail.com>
2024-10-03 20:22:54 -0300

    Service layer, component registration

--- src/main/java/com/ProjetoWebSpring/projetoweb/repositories/UserRepository.java
+++ src/main/java/com/ProjetoWebSpring/projetoweb/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.ProjetoWebSpring.projetoweb.repositories;
 import com.ProjetoWebSpring.projetoweb.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
